229fa0b89b83a25fe333364b33fa66a069a1aff7
Vincent Lidebo Kjellman <vincent.lidebo.kjellman@gmail.com>
2019-04-17 20:13:25 +0200

    Added more dependencies also started on the header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import styled from 'styled-components';
+
+
+const Header = (props) =>{
+    <h1></h1>
+
+}
+
+export default Header;
